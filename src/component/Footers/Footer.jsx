7bee074874a1696b16067c199316ae5a09221337
Gadar-Kumar <gadark144@gmail.com>
2025-08-20 08:38:57 +0530

    css2

--- src/component/Footers/Footer.jsx
+++ src/component/Footers/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      <footer className='bg-gray-900  flex items-center justify-between max-w-screen-xl rounded-b-2xl bottom-0 h-10'>
+    <div className='sticky z-40 bottom-0'>
+      <footer className='bg-gray-600  flex items-center justify-between max-w-screen-xl rounded-b-2xl bottom-0 h-10'>
         <div className='container mx-auto text-center'>
           <p className='text-gray-300'>© 2023 Your Website. All rights reserved.</p>
         </div>
--- src/component/Headers/Header.jsx
+++ src/component/Headers/Header.jsx
@@ -6,8 +6,23 @@ function Header() {
   return (
    
       <header className='shadow sticky z-50 top-0 '>
-          <nav className='bg-gray-100 border-gray-400 px-4 lg:px-6 py-2.5'>
+          <nav className='bg-gray-100 border-gray-400 py-1'>
         <div className='flex flex-wrap justify-between items-center '>
+          <div className='flex justify-end w-100 h-8 text-center'>
+              <input
+              type="text"
+              placeholder='search'
+              className='bg-gray-400 w-100 h-8 ml-0 rounded-md text-center'
+              />
+               <span className='absolute  flex items-center pointer-events-none m-1 mr-2'>
+          {/* Search icon (Heroicons) */}
+          <svg className='h-5 w-5 text-gray-500' xmlns='http://www.w3.org/2000/svg' fill='none'
+               viewBox='0 0 24 24' stroke='currentColor'>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2}
+                  d='M21 21l-4.35-4.35m0 0A7.5 7.5 0 1010.5 18a7.5 7.5 0 006.15-3.35z'/>
+          </svg>
+        </span>
+          </div>
             <ul className='flex space-x-4'>
                 <li>
                     <Link to='/' 
